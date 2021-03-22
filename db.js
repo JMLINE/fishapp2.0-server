9de4a7bbc57a7e1c86fcbf9a90f47a0fd0742289
Jason Line <jasonline87@gmail.com>
2021-03-22 13:44:29 -0400

    test

--- db.js
+++ db.js
@@ -1,15 +1,12 @@
-  
-const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  ​​​​​​​​
   dialect: 'postgres',
-})
-  sequelize.authenticate().then(
-    function() {
-      console.log("Connected to fished postgres database");
-    },
-    function(err) {
-      console.log(err);
-    }
-  );
-
-  module.exports= sequelize
+  dialectOptions: {
+    ​​​​​​​​
+    ssl: {
+      ​​​​​​​​
+      require: true,
+      rejectUnauthorized: false, // very important
+    }​​​​​​​​
+  }​​​​​​​​
+}​​​​​​​​)
