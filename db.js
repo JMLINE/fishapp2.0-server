a4c1e957b975c443494c45a0624406dd4c920032
Jason Line <jasonline87@gmail.com>
2021-03-26 12:53:02 -0400

    d

--- db.js
+++ db.js
@@ -1,21 +1,28 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
+
+// const sequelize = new Sequelize('pettinder', 'postgres', process.env.DB_SECRET, {
+//     host: 'localhost',
+//     dialect: 'postgres'
+// });
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorize: false
-  //   }
-  // }
-})
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 sequelize.authenticate().then(
   function () {
-    console.log('Connected to fishing-app postgres database');
+    console.log('Connected to pettinder postgres database.');
   },
   function (err) {
     console.log(err);
   }
 );
+
 module.exports = sequelize;
