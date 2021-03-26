10bddc780c701c2cf3a96e93a3d655273b5cb790
Jason Line <jasonline87@gmail.com>
2021-03-26 13:29:09 -0400

    f

--- models/user.js
+++ models/user.js
@@ -19,18 +19,3 @@ module.exports = (sequelize, DataTypes) => {
     })
     return User;
 }
-
-
-
-
-
-
-
-// module.exports = function (sequelize, DataTypes) {
-//     return sequelize.define('user', {
-
-//         username: DataTypes.STRING,
-//         newEmail: DataTypes.STRING,
-//         passwordhash: DataTypes.STRING
-//     })
-// }
